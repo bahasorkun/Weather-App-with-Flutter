15fe7583bb571080ba2bac656a12da7f8ce9f9c0
Baha Sorkun <114817265+bahasorkun@users.noreply.github.com>
2024-12-09 20:59:18 +0300

    update

---
+++ minimal_weather_app/lib/pages/weather_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:minimal_weather_app/services/weather_service.dart';
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+
+  // api key 
+  // final  _weatherService = WeatherService();
+
+
+  // fetch weather 
+
+
+
+  // weather animations
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
